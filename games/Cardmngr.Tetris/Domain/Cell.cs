e406ae194f051d854a5bcd9e677cba6f5f56a93b
geloidA <89098491+geloidA@users.noreply.github.com>
2024-04-24 13:01:41 +0500

    start games

---
+++ games/Cardmngr.Tetris/Domain/Cell.cs
@@ -0,0 +1,8 @@
+﻿namespace Cardmngr.Tetris.Domain;
+
+public class Cell(int row, int column, string? cssClass = null)
+{
+    public int Row { get; set; } = row;
+    public int Column { get; set; } = column;
+    public string? CssClass { get; set; } = cssClass;
+}
---
+++ games/Cardmngr.Tetris/Domain/CellCollection.cs
@@ -0,0 +1,28 @@
+﻿namespace Cardmngr.Tetris.Domain;
+
+public class CellCollection
+{
+    private readonly List<Cell> cells = [];
+
+    public bool Contains(int row, int column) => cells.Any(x => x.Row == row && x.Column == column);
+
+    public bool HasColumn(int column) => cells.Any(x => x.Column == column);
+    
+    public bool HasRow(int row) => cells.Any(x => x.Row == row);
+
+    public void Add(int row, int column) => cells.Add(new Cell(row, column));
+
+    public void AddMany(List<Cell> cells, string cssClass) => cells.ForEach(c => this.cells.Add(new Cell(c.Row, c.Column, cssClass)));
+
+    public List<Cell> GetAll() => cells;
+
+    public void SetCssClass(int row, string cssClass) => cells
+        .Where(x => x.Row == row)
+        .ToList()
+        .ForEach(x => x.CssClass = cssClass);
+
+    public void CollapseRows(List<int> rows)
+    {
+        cells.RemoveAll(x => rows.Contains(x.Row));
+    }
+}
